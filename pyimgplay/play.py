ce2ed4a333e81e129e9b276a6f5620c749d2c134
code <server86@163.com>
2011-09-22 11:06:05 +0800

    play fix

--- pyimgplay/play.py
+++ pyimgplay/play.py
@@ -1,61 +1,19 @@
 # coding: utf-8
-
 import os
 import time
-from PIL import Image
 
-LEN = 78
-SIZE = (78, 36)
-
-
-def resize_img():
-    """resize images
-    """
-    lst = get_files("jpeg")
-    for x in lst:
-        img = Image.open(x)
-        out = img.resize(SIZE)
-        out.save(x[:-4] + "jpg")
-
-    os.system("rm *.jpeg")
-
-
-def get_files(suffix):
-    """get files by suffix
-    """
-    lst = os.listdir(".")
-    return filter(lambda x: x.endswith(suffix), lst)
-
-
-def gen_char_img(data, t_name):
-    """generate char image
-    """
-    position = 1
-    with open(t_name, "w") as f:
-        for x in data:
-            if position > LEN :
-                f.write("\n")
-                position = 1
-            # char style
-            if sum(x):
-                if sum(x) > 100: f.write("*")
-                else: f.write("%")
-            else:
-                f.write(" ")
-            position += 1
-
-    os.system("rm " + t_name[:-3] + "jpg")
-    print t_name, " done ..."
-
-
-def main():
-    resize_img()
-    lst = get_files("jpg")
-    for f in lst:
-        img = Image.open(f)
-        r = img.getdata()
-        gen_char_img(r, f[:-3] + "txt")
-
+def playwav():
+    time.sleep(1)
+    os.system("cvlc 0.wav >& /dev/null &")
 
 if __name__ == "__main__":
-    main()
+    t = 0.1
+    lst = os.listdir(".")
+    lst = filter(lambda x: x[-3:] == "txt", lst)
+    playwav()
+    for n in xrange(1, len(lst) + 1):
+        os.system("clear")
+        with open(str(n) + ".txt") as f:
+            for x in f: print x,
+            print n
+            time.sleep(t)
